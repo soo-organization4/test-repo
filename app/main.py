1f77420616d5619a0552bf2817145e6a1867f695
khs4git3-test <125840973+khs4git3-test@users.noreply.github.com>
2026-06-04 16:47:31 +0900

    원격에서 코드 수정

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ service = VectorService()
 from app.rag_service import RagService 
 rag = RagService()  
 
-st.title("[VS-GitHub] RAG Demo (MariaDB VectorStore 기반)")
+st.title("[Remote-VS-GitHub] RAG Demo (MariaDB VectorStore 기반)")
 
 # 1) 샘플 문서 저장
 if st.button("샘플 문서 저장"):
